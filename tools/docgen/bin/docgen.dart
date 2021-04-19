7a0c487e33792e5c3640308afa0b6bbf918a0855
Noah Cain <nmcain@gmail.com>
2021-04-18 23:16:30 -0700

    Add commentary to docgen.dart

--- tools/docgen/bin/docgen.dart
+++ tools/docgen/bin/docgen.dart
@@ -1,4 +1,8 @@
 //developers - use `dart bin/docgen.dart` to create the HTML version of the docs!
+//variables read here:
+//DOCGEN_DOCUMENT_GHLINK_GGFF: This is the link to the document's source code
+//DOCGEN_DOCUMENT_TITLE_GGFF: This is the name of the document, used in the heading and title
+//Declare other in-document variables with the suffix _GGFF to prevent accidental insertion
 import 'dart:io';
 import 'package:markdown/markdown.dart';
 
