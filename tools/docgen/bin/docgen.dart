f2dc8ff39df6789273bccdb72d9651aa6c26cb5b
Noah Cain <nmcain@gmail.com>
2021-04-19 19:11:52 -0700

    Finish document generation

--- tools/docgen/bin/docgen.dart
+++ tools/docgen/bin/docgen.dart
@@ -5,6 +5,7 @@
 //Declare other in-document variables with the suffix _GGFF to prevent accidental insertion
 import 'dart:io';
 import 'package:markdown/markdown.dart';
+import 'dart:convert';
 
 //Relative path to markdown documents
 var relPath = '../../docs/articles/';
@@ -33,7 +34,7 @@ var markdownDocs = [
     'pangolin/pangolin-linux.html'
   ],
   ['install/QEMU.md', 'Running dahliaOS in QEMU', 'install/qemu.html'],
-  ['install/64bit.md', 'dahliaOS 64-bit with UEFI' 'install/efi.html'],
+  ['install/64bit.md', 'dahliaOS 64-bit with UEFI', 'install/efi.html'],
   [
     'install/64bit-Legacy.md',
     'dahliaOS 64-bit with Legacy BIOS',
@@ -53,7 +54,7 @@ var markdownDocs = [
   [
     'developer/packaging.md',
     'Packaging Applications for dahliaOS',
-    'developer/packaging.hmtl'
+    'developer/packaging.html'
   ],
   [
     'developer/submitting.md',
@@ -72,13 +73,29 @@ var markdownDocs = [
   ],
 ];
 
+void createFile(String filename, String contents) {
+  File(filename).createSync(recursive: true);
+  final file = File(filename).writeAsString(contents);
+  print(filename + " was created successfully");
+}
+
 void main(List<String> arguments) {
-  var documentInQuestion =
-      Process.runSync('cat', ['../../docs/articles/os/linux-based.md'])
-          .stdout
-          .toString();
-  print(
-      markdownToHtml(documentInQuestion, inlineSyntaxes: [InlineHtmlSyntax()]));
-  var i = 0;
-  markdownDocs.forEach((e) => print('position for ${i++} is ${e}'));
+  for (var i = 0; i < markdownDocs.length; i++) {
+    var documentInQuestion =
+        Process.runSync('cat', [relPath + markdownDocs[i][0]])
+            .stdout
+            .toString();
+    var header = Process.runSync('cat', ['header.html']).stdout.toString();
+    var footer = Process.runSync('cat', ['footer.html']).stdout.toString();
+
+    var fullDoc = (header +
+            markdownToHtml(documentInQuestion,
+                inlineSyntaxes: [InlineHtmlSyntax()]) +
+            footer)
+        .replaceAll('DOCGEN_DOCUMENT_TITLE_GGFF', markdownDocs[i][1])
+        .replaceAll('DOCGEN_DOCUMENT_GHLINK_GGFF', ghRepo + markdownDocs[i][0]);
+
+    print('Generating $i : ${markdownDocs[i]} ');
+    createFile(generatedDocPath + markdownDocs[i][2], fullDoc);
+  }
 }
