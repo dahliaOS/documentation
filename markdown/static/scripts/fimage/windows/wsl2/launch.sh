96ace7a76b06e92a1b390c07c866d4a3ce19182b
Quinten Van Damme <quintenvandamme@duck.com>
2022-01-09 11:19:35 +0100

    update to fimage-220108

--- markdown/static/scripts/fimage/windows/wsl2/launch.sh
+++ markdown/static/scripts/fimage/windows/wsl2/launch.sh
@@ -4,15 +4,15 @@ while [[ $# -gt 0 ]]; do
   PROG_ARGS+=("${1}")
   case "${1}" in
     -gui|--gui)
-      sudo apt update;sudo apt install git unzip dnsmasq -y;wget https://github.com/dahliaOS/fimage/releases/download/fimage-210525/fimage-210525.zip;unzip fimage-210525.zip;cd fimage-210525;./ffx-linux-x64 platform preflight;./network-config;sudo ./fimage-gui-hostGPU 4096
+      sudo apt update;sudo apt install git unzip dnsmasq -y;wget https://github.com/dahliaOS/fimage/releases/download/fimage-220108/fimage-220108.zip;unzip fimage-220108.zip;cd fimage-220108;./ffx-linux-x64 platform preflight;./network-config;sudo ./fimage-gui-hostGPU 4096
       break
       ;;
     -cli|--cli)
-      sudo apt update;sudo apt install git unzip dnsmasq -y;wget https://github.com/dahliaOS/fimage/releases/download/fimage-210525/fimage-210525.zip;unzip fimage-210525.zip;cd fimage-210525;./ffx-linux-x64 platform preflight;./network-config;sudo ./fimage-headless 4096
+      sudo apt update;sudo apt install git unzip dnsmasq -y;wget https://github.com/dahliaOS/fimage/releases/download/fimage-220108/fimage-220108.zip;unzip fimage-220108.zip;cd fimage-220108;./ffx-linux-x64 platform preflight;./network-config;sudo ./fimage-headless 4096
       break
       ;;
     -reset|--reset)
-      sudo rm -r fimage-210525/;sudo rm fimage-210525.zip
+      sudo rm -r fimage-220108/;sudo rm fimage-220108.zip
       break
       ;;
         esac
