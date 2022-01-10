e9cf4c3c722504a443edf0c944a509e5d81ea6d0
Quinten Van Damme <quintenvandamme@duck.com>
2022-01-10 18:42:13 +0100

    fix create changes

--- tools/changes/main.py
+++ tools/changes/main.py
@@ -1,5 +1,21 @@
-from lib import make_release_file
+import os
+import datetime as DT
 
-repo = 'documentation' # set repo
+today = DT.date.today()
+week_ago = today - DT.timedelta(days=7)
+DATE_SYSTEM = today.strftime("%y%m%d")
+DATE_TAG = week_ago.strftime("%y%m%d")
 
-make_release_file.main(repo) # make release file
+os.system('git pull')
+os.system('rm .github/release.md')
+os.system('echo "# dahliaOS documentation" >> .github/release.md')
+os.system('echo "" >> .github/release.md')
+os.system('echo "## changes" >> .github/release.md')
+os.system('echo "" >> .github/release.md')
+os.system('echo "Compare to previous release [here](https://github.com/dahliaOS/documentation/compare/v'+str(DATE_TAG)+'...v'+str(DATE_SYSTEM)+')." >> .github/release.md')
+os.system('cd ..')
+os.system('git config --global user.name "dahliaOS"')
+os.system('git config --global user.email contact@dahliaos.io')
+os.system('git add --all')
+os.system('git commit -m "Bump changes to '+'v'+str(DATE_SYSTEM)+'"')
+os.system('git push')
