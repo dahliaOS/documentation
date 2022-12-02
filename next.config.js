874283b65d7cc98e8d18d91d8b7369aedfb6bb76
SincerelyFaust <hedesmarin@gmail.com>
2022-12-02 19:27:41 +0100

    fix(app): nextra 2.0, optimize images

--- next.config.js
+++ next.config.js
@@ -2,16 +2,12 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
 };
 
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-  unstable_staticImage: true,
-  unstable_flexsearch: {
-    codeblocks: true,
-  },
-  unstable_defaultShowCopyCode: true,
 });
 
 module.exports = withNextra({
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,11 @@
 import { useTheme } from "nextra-theme-docs";
 import { useState, useEffect } from "react";
-import Vercel from "./components/Vercel";
+import { VercelLogo } from "./components/Icons";
+import Image from "next/image";
+import lightLogotype from "./public/images/logos/lightLogotype.webp";
+import darkLogotype from "./public/images/logos/darkLogotype.webp";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const useDark = () => {
   const { resolvedTheme } = useTheme();
@@ -16,22 +21,11 @@ const useDark = () => {
 
 const config = {
   project: {
-    link: "https://dahliaOS.io",
-    icon: () => {
-      const isDark = useDark();
-      return (
-        <>
-          <img
-            src={`/logos/${isDark ? "dark" : "light"}Logomark.svg`}
-            width="22"
-            alt="dahliaOS"
-          />
-        </>
-      );
-    },
+    link: "https://dahliaos.io/github",
   },
-  projectChat: { link: "https://dahliaos.io/discord" },
-  github: "https://github.com/dahliaOS",
+  chat: {
+    link: "https://dahliaos.io/discord",
+  },
   docsRepositoryBase: "https://github.com/dahliaOS/documentation",
   titleSuffix: " – dahliaOS",
   unstable_staticImage: true,
@@ -39,88 +33,135 @@ const config = {
   toc: {
     extraContent: () => {
       return (
-        <a target="_blank" rel="noreferrer" href="https://dahliaos.io/donate">
-          Support dahliaOS 💝 →
-        </a>
+        <Link target="_blank" href="https://dahliaos.io/donate">
+          💝 Support dahliaOS →
+        </Link>
       );
     },
     float: true,
   },
-  feedback: { link: "Question? Give us feedback →", labels: "feedback" },
+  feedback: { content: "❓ Question? Give us feedback →", labels: "feedback" },
   navigation: { next: true, prev: true },
   unstable_flexsearch: true,
   darkMode: true,
   banner: {
     text: () => {
       return (
-        <a
+        <Link
           href="https://dahliaOS.io/download"
           target="_blank"
-          rel="noopener noreferrer"
           title="Download the latest version of dahliaOS"
         >
           dahliaOS Linux 220222 is out →
-        </a>
+        </Link>
       );
     },
-    key: "dahliaOS",
+    key: "dahliaOS new release",
   },
   footer: {
     text: () => {
       return (
         <>
-          <a href="https://www.apache.org/licenses/LICENSE-2.0">Copyright</a> -
-          2019 - {new Date().getFullYear()} @{" "}
-          <a href="mailto:contact@dahliaos.io">The dahliaOS Authors</a>
-          <p>
-            <a
-              href="https://vercel.com/?utm_source=dahliaOS&utm_campaign=osss"
+          <div
+            style={{ display: "flex", flexDirection: "column", gap: "10px" }}
+          >
+            <p>
+              Copyright - 2019 - {new Date().getFullYear()} @ The dahliaOS
+              Authors
+            </p>
+            <Link
+              href="https://vercel.com?utm_source=dahliaOS&amp;utm_campaign=oss"
               target="_blank"
-              rel="noreferrer"
+              style={{ display: "flex", flexDirection: "row", gap: "10px" }}
             >
-              <span>Powered By</span>
-              <span>
-                <Vercel />
-              </span>
-            </a>
-          </p>
+              Powered By
+              <VercelLogo />
+            </Link>
+          </div>
         </>
       );
     },
   },
   editLink: {
-    text: "Edit this page on GitHub →",
+    text: "✍️ Edit this page on GitHub →",
   },
   logo: () => {
     const isDark = useDark();
     return (
       <>
-        <img
-          src={`/logos/${isDark ? "dark" : "light"}Logotype.svg`}
+        <Image
+          src={isDark ? darkLogotype : lightLogotype}
           width="120"
           alt="dahliaOS"
           style={{ marginRight: "10px" }}
         />
-        <span>Documentation</span>
+        <span style={{ fontWeight: "600" }}>Documentation</span>
       </>
     );
+  },
+  useNextSeoProps() {
+    const { route } = useRouter();
+    if (route !== "/") {
+      return {
+        titleTemplate: "%s – dahliaOS Docs",
+      };
+    }
   },
   head: () => {
     return (
       <>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-title" content="Documentation" />
         <link rel="canonical" href="https://docs.dahliaos.io" />
-        <meta name="viewport" content="width=device-width" />
-        <meta property="og:image" content="https://imgur.com/pqgjEpd.png" />
-        <meta property="og:site_name" content="dahliaOS" />
-        <meta property="og:title" content="Documentation" key="title" />
-        <meta name="description" content="Documentation for dahliaOS." />
-        <meta name="og:description" content="Documentation for dahliaOS." />
         <meta property="og:url" content="https://docs.dahliaos.io" />
         <meta name="theme-color" content="#ff3d00" />
+        <meta property="og:image" content="https://imgur.com/pqgjEpd.png" />
+        <meta property="og:site_name" content="dahliaOS" />
+        <meta name="viewport" content="width=device-width" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/images/favicon/favicon-32x32.png"
+        />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#ff3d00" />
+        <meta name="apple-mobile-web-app-title" content="dahliaOS" />
+        <meta
+          name="msapplication-TileImage"
+          content="/images/favicon/favicon.png"
+        />
+        <meta name="msapplication-TileColor" content="#ff3d00" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta
+          name="keywords"
+          content="dahlia, documentation, software, flutter, fuchsia, linux, dahliaOS, computer, operating system, os, system, kernel, dart, open source, material, design, zircon, go, rust"
+        />
+        <meta
+          name="description"
+          content="dahliaOS is a modern, secure, lightweight and responsive operating system, combining the best of GNU/Linux and Fuchsia OS."
+        />
+        <meta
+          name="og:description"
+          content="dahliaOS is a modern, secure, lightweight and responsive operating system, combining the best of GNU/Linux and Fuchsia OS."
+        />
       </>
     );
   },
