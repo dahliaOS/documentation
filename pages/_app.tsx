858047aed12d22f764d76063f7357ee748ef3dc1
SincerelyFaust <hedesmarin@gmail.com>
2023-02-23 17:16:38 +0100

    feat(docs): add cards

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import type { ReactElement } from "react";
 import type { AppProps } from "next/app";
 
-import "../styles/globals.css";
-
 export default function Nextra({
   Component,
   pageProps,
