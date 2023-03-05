19be67e2acf886d9ff1b7df5b17ab1659cd13f26
SincerelyFaust <hedesmarin@gmail.com>
2023-03-05 13:14:19 +0100

    fix(public): optimize images, run prettier

--- theme.config.tsx
+++ theme.config.tsx
@@ -65,7 +65,10 @@ const config = {
           >
             <p>
               Copyright - 2019 - {new Date().getFullYear()} @ The dahliaOS
-              Authors - This project is licensed under the <a href="https://github.com/dahliaOS/documentation/blob/main/LICENSE">Apache 2.0 license</a>
+              Authors - This project is licensed under the{" "}
+              <a href="https://github.com/dahliaOS/documentation/blob/main/LICENSE">
+                Apache 2.0 license
+              </a>
             </p>
             <Link
               href="https://vercel.com?utm_source=dahliaOS&amp;utm_campaign=oss"
